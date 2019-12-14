c87370b2b7d321bb6ae04b0a059ed9bd45813d53
htlifelover <htlifelover@gmail.com>
2019-12-13 23:53:33 -0500

    Initial Commit

---
+++ Kitchen-Master/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kitchen-Master
+//
+//  Created by Zhihao Wang on 12/13/19.
+//  Copyright © 2019 Zhihao Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
