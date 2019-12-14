67b1a099f673237f4f54215cfada5f37f4ce7958
htlifelover <htlifelover@gmail.com>
2019-12-14 07:15:15 -0500

    first commit

---
+++ Kitchen-Master/Recipe.swift
@@ -0,0 +1,9 @@
+//
+//  Recipe.swift
+//  Kitchen-Master
+//
+//  Created by Zhihao Wang on 12/13/19.
+//  Copyright © 2019 Zhihao Wang. All rights reserved.
+//
+
+import Foundation
---
+++ Kitchen-Master/recipeDB.swift
@@ -0,0 +1,9 @@
+//
+//  recipeDB.swift
+//  Kitchen-Master
+//
+//  Created by Zhihao Wang on 12/14/19.
+//  Copyright © 2019 Zhihao Wang. All rights reserved.
+//
+
+import Foundation
